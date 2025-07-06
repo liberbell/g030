192483db595504826e2095b15a16dad693a57713
Liberbell <liberbell@gmail.com>
2025-07-07 08:12:56 +0900

    render2

--- lesson1/go-stripe/cmd/web/render.go
+++ lesson1/go-stripe/cmd/web/render.go
@@ -27,3 +27,7 @@ var templateFS embed.FS
 func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
 	return td
 }
+
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+}
