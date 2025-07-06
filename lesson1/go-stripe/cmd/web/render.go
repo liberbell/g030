4bd047b71b2ff3b60ac0de0f1d9e566ef3363252
Liberbell <liberbell@gmail.com>
2025-07-06 14:31:33 +0900

    render2

--- lesson1/go-stripe/cmd/web/render.go
+++ lesson1/go-stripe/cmd/web/render.go
@@ -4,4 +4,9 @@ type templateData struct {
 	StringMap map[string]string
 	IntMap    map[string]int
 	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
 }
