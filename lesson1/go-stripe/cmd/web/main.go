6771e9b9a8ccd918d21dbf694c35ab661cfe4573
Liberbell <liberbell@gmail.com>
2025-07-06 14:08:37 +0900

    main1

--- lesson1/go-stripe/cmd/web/main.go
+++ lesson1/go-stripe/cmd/web/main.go
@@ -34,7 +34,7 @@ type application struct {
 	version       string
 }
 
-func (app *application) serve() {
+func (app *application) serve() error {
 	srv := &http.Server{
 		Addr:              fmt.Sprintf(":%d", app.config.port),
 		Handler:           app.routes(),
