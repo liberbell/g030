9626cb779eb9db0823773f3f2b58ead12b869e06
Liberbell <liberbell@gmail.com>
2025-07-09 08:27:38 +0900

    handlers.Please3

--- lesson1/go-stripe/cmd/web/handlers.go
+++ lesson1/go-stripe/cmd/web/handlers.go
@@ -4,4 +4,9 @@ import "net/http"
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	app.infoLog.Println("hit the handler")
+	if err != nil {
+		if err != app.renderTemplate(w, r, "terminlal", nil); err != nil {
+			app.infoLog.Println("hit the handler")}
+		}
+	}
 }
