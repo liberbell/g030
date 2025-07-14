78104f508bf6c45bc051a2c1d5c1551ab65756b4
Liberbell <liberbell@gmail.com>
2025-07-15 08:11:48 +0900

    api1

--- lesson1/go-stripe/cmd/api/api.go
+++ lesson1/go-stripe/cmd/api/api.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"html/template"
+	"log"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
+
 func main() {
 
 }
