c87227e1d782a573c6d7720dd18db961600388be
Liberbell <liberbell@gmail.com>
2025-07-24 14:13:32 +0900

    main1

--- lesson1/go-stripe/cmd/web/main.go
+++ lesson1/go-stripe/cmd/web/main.go
@@ -54,6 +54,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server Port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "james:jamespass")
 	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
 
 	flag.Parse()
