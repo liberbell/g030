ef265d227ea4fb85c2581d9bb05d76159f633f55
Liberbell <liberbell@gmail.com>
2025-07-26 10:48:15 +0900

    routes2

--- lesson1/go-stripe/cmd/web/routes.go
+++ lesson1/go-stripe/cmd/web/routes.go
@@ -13,6 +13,7 @@ func (app *application) routes() http.Handler {
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 
 	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
