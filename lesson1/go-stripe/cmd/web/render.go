458e619819d1650c81c4160a6d5cf5a7e8396b56
Liberbell <liberbell@gmail.com>
2025-07-29 08:23:58 +0900

    render3

--- lesson1/go-stripe/cmd/web/render.go
+++ lesson1/go-stripe/cmd/web/render.go
@@ -22,7 +22,9 @@ type templateData struct {
 	CSSVersion      string
 }
 
-var functions = template.FuncMap{}
+var functions = template.FuncMap{
+	"formatCurrency": formatCurrency,
+}
 
 func formatCurrency(n int) string {
 	f := float32(n / 100)
