0a00f6ea6a5ae76aef5aa516f1c057c58f55fa47
Liberbell <liberbell@gmail.com>
2025-07-30 08:41:36 +0900

    handlers-api2

--- lesson1/go-stripe/cmd/api/handlers-api.go
+++ lesson1/go-stripe/cmd/api/handlers-api.go
@@ -76,4 +76,5 @@ func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request)
 
 func (app *application) GetWidgetbyID(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
+	widgetID, _ := strconv.Atoi(id)
 }
