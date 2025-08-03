a6e39615ab74199c6e224cbd635b608151112a43
Liberbell <liberbell@gmail.com>
2025-08-04 08:24:50 +0900

    handlers2

--- lesson1/go-stripe/cmd/web/handlers.go
+++ lesson1/go-stripe/cmd/web/handlers.go
@@ -65,6 +65,7 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 	data["last_four"] = lastFour
 	data["expiry_month"] = expiryMonth
 	data["expiry_year"] = expiryMonth
+	data["bank_return_code"] = pi.Charges.Data[0].ID
 
 	if err := app.renderTemplate(w, r, "succeeded", &templateData{
 		Data: data,
