d7c1788fb1c67379a30631e85e90da29ba5cf898
Liberbell <liberbell@gmail.com>
2025-08-05 08:28:27 +0900

    handlers2

--- lesson1/go-stripe/cmd/web/handlers.go
+++ lesson1/go-stripe/cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", &templateData{}); err != nil {
+	if err := app.renderTemplate(w, r, "home", &templateData{}); err != nil {
 		app.errorLog.Println(err)
 	}
 }
