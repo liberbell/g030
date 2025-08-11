608e145c493d58edc3b41b302cc21c2f354da373
Liberbell <liberbell@gmail.com>
2025-08-12 08:33:55 +0900

    routes2

--- lesson1/go-stripe/cmd/web/routes.go
+++ lesson1/go-stripe/cmd/web/routes.go
@@ -12,7 +12,8 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
-	mux.Post("/virtual-terminal-payment-succeeded", app.PaymentSucceeded)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
 
 	mux.Get("/receipt", app.Receipt)
 
