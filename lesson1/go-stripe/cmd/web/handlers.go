8640c4a9698365688e8377589827e4a4b1bb34c9
Liberbell <liberbell@gmail.com>
2025-08-14 08:39:53 +0900

    handlers1

--- lesson1/go-stripe/cmd/web/handlers.go
+++ lesson1/go-stripe/cmd/web/handlers.go
@@ -256,8 +256,13 @@ func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) BronzePlan(w http.ResponseWriter, r *http.Request) {
-	intMap := make(map[string]int)
-	intMap["plan_id"] = 1
+	widget, err := app.DB.GetWidget(2)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	data := make(map[string]interface{}, 0)
 	if err := app.renderTemplate(w, r, "bronze-plan", &templateData{
 		IntMap: intMap,
 	}); err != nil {
