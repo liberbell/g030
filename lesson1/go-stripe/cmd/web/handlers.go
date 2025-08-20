0629eb7d730f6ae1f3e4e6c46725a46a86b9c386
Liberbell <liberbell@gmail.com>
2025-08-21 08:13:48 +0900

    handlers1

--- lesson1/go-stripe/cmd/web/handlers.go
+++ lesson1/go-stripe/cmd/web/handlers.go
@@ -270,3 +270,19 @@ func (app *application) BronzePlan(w http.ResponseWriter, r *http.Request) {
 		app.errorLog.Print(err)
 	}
 }
+
+func (app *application) BronzePlanReceipt(w http.ResponseWriter, r *http.Request) {
+	widget, err := app.DB.GetWidget(2)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+	if err := app.renderTemplate(w, r, "bronze-plan", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Print(err)
+	}
+}
