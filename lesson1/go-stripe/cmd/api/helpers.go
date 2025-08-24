84dca6c0368dc6ae2047b70014a74d8b69534a8e
Liberbell <liberbell@gmail.com>
2025-08-24 18:43:47 +0900

    handlers1

--- lesson1/go-stripe/cmd/api/helpers.go
+++ lesson1/go-stripe/cmd/api/helpers.go
@@ -1,9 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	maxBytes := 1048576
 
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
 }
