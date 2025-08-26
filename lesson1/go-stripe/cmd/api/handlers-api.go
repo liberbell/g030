d23657ed544329a52355c12179cc19004961f2e1
Liberbell <liberbell@gmail.com>
2025-08-27 08:43:54 +0900

    gostripe1

--- lesson1/go-stripe/cmd/api/handlers-api.go
+++ lesson1/go-stripe/cmd/api/handlers-api.go
@@ -266,7 +266,7 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 	var payload struct {
 		Error   bool          `json: "error"`
 		Message string        `json: "message"`
-		Token   *models.Token `json: 'authentication_token"`
+		Token   *models.Token `json: "authentication_token"`
 	}
 	payload.Error = false
 	payload.Message = fmt.Sprintf("token for %s created", userInput.Email)
