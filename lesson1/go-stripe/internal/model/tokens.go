9be02dc019c1bd827343b34511cfce002a17b26d
Liberbell <liberbell@gmail.com>
2025-08-28 08:57:01 +0900

    tokens3

--- lesson1/go-stripe/internal/model/tokens.go
+++ lesson1/go-stripe/internal/model/tokens.go
@@ -42,6 +42,12 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	stmt := `DELETE FROM tokens WHERE user_id = ?`
+	_, err := m.DB.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
 	stmt := `INSERT INTO tokens
 				(user_id, name, email, token_hash, created_at, updated_at)
 			 Values (?, ?, ?, ?, ?, ?)`
