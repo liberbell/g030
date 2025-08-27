2a0e0ec6b9488758a49891ae20a322008e660a42
Liberbell <liberbell@gmail.com>
2025-08-28 08:57:49 +0900

    tokens4

--- lesson1/go-stripe/internal/model/tokens.go
+++ lesson1/go-stripe/internal/model/tokens.go
@@ -48,11 +48,11 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 		return err
 	}
 
-	stmt := `INSERT INTO tokens
+	stmt = `INSERT INTO tokens
 				(user_id, name, email, token_hash, created_at, updated_at)
 			 Values (?, ?, ?, ?, ?, ?)`
 
-	_, err := m.DB.ExecContext(ctx, stmt, u.ID, u.LastName, u.Email, t.Hash, time.Now(), time.Now())
+	_, err = m.DB.ExecContext(ctx, stmt, u.ID, u.LastName, u.Email, t.Hash, time.Now(), time.Now())
 	if err != nil {
 		return err
 	}
