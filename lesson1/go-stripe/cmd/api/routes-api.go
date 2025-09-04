e0f2651017d9884ffa81bcddd029a8c19f60e65c
Liberbell <liberbell@gmail.com>
2025-09-05 08:51:35 +0900

    routes-api2

--- lesson1/go-stripe/cmd/api/routes-api.go
+++ lesson1/go-stripe/cmd/api/routes-api.go
@@ -27,5 +27,7 @@ func (app *application) routes() http.Handler {
 	mux.Post("/api/authenticate", app.CreateAuthToken)
 	mux.Post("/api/is-authenticated", app.CheckAuthentication)
 
+	mux.Route("/api/admin", func(mux chi.Router) {})
+
 	return mux
 }
