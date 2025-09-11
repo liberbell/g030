54a88d55fc8d6e2a6172243b900a73331c11a790
Liberbell <liberbell@gmail.com>
2025-09-11 18:44:42 +0900

    middleware1

--- lesson1/go-stripe/cmd/web/middleware.go
+++ lesson1/go-stripe/cmd/web/middleware.go
@@ -5,3 +5,14 @@ import "net/http"
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
 }
+
+func (app *application) Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := app.authenticateToken(r)
+		if err != nil {
+			app.invalidCredentials(w)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
