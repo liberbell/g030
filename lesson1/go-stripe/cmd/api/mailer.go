94cd85537992937df62ad1e4bf1bafb2b0963406
Liberbell <liberbell@gmail.com>
2025-09-15 12:23:47 +0900

    mailer3

--- lesson1/go-stripe/cmd/api/mailer.go
+++ lesson1/go-stripe/cmd/api/mailer.go
@@ -1,11 +1,21 @@
 package main
 
-import "embed"
+import (
+	"embed"
+	"fmt"
+	"html/template"
+)
 
 //go:embed templates
 var emailTemplatesFS embed.FS
 
 func (app *application) SendMail(from, to, subject, tmpl string, data interface{}) error {
+	templateToRender := fmt.Sprintf("templates/%s.html.tmpl")
 
+	t, err := template.New("email-html").ParseFS(emailTemplatesFS, templateToRender)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
 	return nil
 }
