f8efe06707105404a27659929c07f60147e80fe7
Liberbell <liberbell@gmail.com>
2025-09-20 07:46:07 +0900

    signer5

--- lesson1/go-stripe/internal/urlsigner/signer.go
+++ lesson1/go-stripe/internal/urlsigner/signer.go
@@ -1,11 +1,17 @@
 package urlsigner
 
+import (
+	goalone "github.com/bwmarrin/go-alone"
+)
+
 type Signer struct {
 	Secret []byte
 }
 
 func (s *Signer) GenerateTokenFromString(data string) string {
 	var urlToSign string
+
+	crypt := goalone.New(s.Secret, goalone.Timestamp)
 }
 
 func (s *Signer) VeryfyToken(token string) bool {
