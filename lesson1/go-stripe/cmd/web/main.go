e1e3cbc58704bf93ea9a92f0ba70d71d8599699a
Liberbell <liberbell@gmail.com>
2025-09-21 20:10:02 +0900

    main1

--- lesson1/go-stripe/cmd/web/main.go
+++ lesson1/go-stripe/cmd/web/main.go
@@ -32,6 +32,8 @@ type config struct {
 		secret string
 		key    string
 	}
+	secretkey string
+	frontend  string
 }
 
 type application struct {
@@ -67,6 +69,8 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
 	flag.StringVar(&cfg.db.dsn, "dsn", "james:jamespass@tcp(127.0.0.1:3306)/widgets?parseTime=true&tls=false", "DSN")
 	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
+	flag.StringVar(&cfg.secretkey, "secret", "elsiespq1k", "secret key")
+	flag.StringVar(&cfg.frontend, "frontend", "http://localhost:4000", "url to front end")
 
 	flag.Parse()
 
