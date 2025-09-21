4c5391d0fdbe7b796d755dfd1663e2bbbcf16705
Liberbell <liberbell@gmail.com>
2025-09-21 19:57:02 +0900

    handlers-api3

--- lesson1/go-stripe/cmd/api/handlers-api.go
+++ lesson1/go-stripe/cmd/api/handlers-api.go
@@ -394,6 +394,18 @@ func (app *application) SendPasswordResetEmail(w http.ResponseWriter, r *http.Re
 		return
 	}
 
+	_, err = app.DB.GetUserByEmail(payload.Email)
+	if err != nil {
+		var resp struct {
+			Error   bool   `json: "error"`
+			Message string `json: "message"`
+		}
+		resp.Error = true
+		resp.Message = "No matching email found on our system"
+		app.writeJSON(w, http.StatusAccepted, resp)
+		return
+	}
+
 	link := fmt.Sprintf("%s/reset-password?email=%s", app.config.frontend, payload.Email)
 	sign := urlsigner.Signer{
 		Secret: []byte(app.config.secretkey),
