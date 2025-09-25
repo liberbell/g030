c802f5734391e7831b91e6b7d947678515021293
Liberbell <liberbell@gmail.com>
2025-09-26 08:46:45 +0900

    encryption4

--- lesson1/go-stripe/internal/encryption/encryption.go
+++ lesson1/go-stripe/internal/encryption/encryption.go
@@ -1,13 +1,19 @@
 package encryption
 
+import "crypto/aes"
+
 type Encryption struct {
 	Key []byte
 }
 
 func (e *Encryption) Encrypt(text string) (string, error) {
-
+	plaintext := []byte(text)
+	block, err := aes.NewCipher(e.Key)
+	if err != nil {
+		return "", err
+	}
 }
 
-func (e *Encryption) Decrypt(text string) (string, error) {
+func (e *Encryption) Decrypt(cryptoText string) (string, error) {
 
 }
