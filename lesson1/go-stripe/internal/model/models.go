c6b63490c0f27244e97c54ce512c6bbed99f90d4
Liberbell <liberbell@gmail.com>
2025-09-30 08:23:45 +0900

    models1

--- lesson1/go-stripe/internal/model/models.go
+++ lesson1/go-stripe/internal/model/models.go
@@ -38,15 +38,18 @@ type Widget struct {
 }
 
 type Order struct {
-	ID            int       `json: "id"`
-	WidgetID      int       `json: "widget_id"`
-	TransactionID int       `json: "transaction_id"`
-	CustomerID    int       `json: "customer_id"`
-	StatusID      int       `json: "status_id"`
-	Quantity      int       `json: "quantity"`
-	Amount        int       `json: "amount"`
-	CreatedAt     time.Time `json: "-"`
-	UpdatedAt     time.Time `json: "-"`
+	ID            int         `json: "id"`
+	WidgetID      int         `json: "widget_id"`
+	TransactionID int         `json: "transaction_id"`
+	CustomerID    int         `json: "customer_id"`
+	StatusID      int         `json: "status_id"`
+	Quantity      int         `json: "quantity"`
+	Amount        int         `json: "amount"`
+	CreatedAt     time.Time   `json: "-"`
+	UpdatedAt     time.Time   `json: "-"`
+	Widget        Widget      `json: "widget"`
+	Transaction   Transaction `json: "transaction"`
+	Customer      Customer    `json: "customer"`
 }
 
 type Status struct {
