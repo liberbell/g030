992347321a68ac804beee94b4c09729917f58a90
Liberbell <liberbell@gmail.com>
2025-10-09 08:40:02 +0900

    routes-api2

--- lesson1/go-stripe/cmd/api/routes-api.go
+++ lesson1/go-stripe/cmd/api/routes-api.go
@@ -33,7 +33,7 @@ func (app *application) routes() http.Handler {
 		mux.Use(app.Auth)
 
 		mux.Post("/virtual-terminal-succeeded", app.VirtualTerminalPaymentSucceeded)
-		mux.Post("/all-sales", app.allSales)
+		mux.Post("/all-sales", app.AllSales)
 		mux.Post("/all-subscriptions", app.AllSubscriptions)
 
 		mux.Post("/get-sales/{id}", app.GetSale)
