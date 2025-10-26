bd3a522dd6630c2dbcc173b6eb8f6ccb9297e856
Liberbell <liberbell@gmail.com>
2025-10-26 17:19:32 +0900

    routes-api1

--- lesson1/go-stripe/cmd/api/routes-api.go
+++ lesson1/go-stripe/cmd/api/routes-api.go
@@ -40,6 +40,8 @@ func (app *application) routes() http.Handler {
 
 		mux.Post("/refund", app.RefundCharge)
 		mux.Post("/cancel-subscription", app.CancelSubscription)
+
+		mux.Post("/all-users", app.AllUsers)
 	})
 
 	return mux
