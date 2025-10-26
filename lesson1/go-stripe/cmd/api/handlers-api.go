e1f4d480fe6bdf846980c15d5f2e8473a94dee1f
Liberbell <liberbell@gmail.com>
2025-10-26 17:18:24 +0900

    handlers-api1

--- lesson1/go-stripe/cmd/api/handlers-api.go
+++ lesson1/go-stripe/cmd/api/handlers-api.go
@@ -625,3 +625,12 @@ func (app *application) CancelSubscription(w http.ResponseWriter, r *http.Reques
 
 	app.writeJSON(w, http.StatusOK, resp)
 }
+
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	allUsers, err := app.DB.GetAllUsers()
+	if err != nil {
+		app.badRequest(w, r, err)
+	}
+
+	app.writeJSON(w, http.StatusOK, allUsers)
+}
