ec0201ff33f9bb55952470be9afedeab8bcf4b81
Liberbell <liberbell@gmail.com>
2025-11-03 14:17:11 +0900

    ws-handlers2

--- lesson1/go-stripe/cmd/web/ws-handlers.go
+++ lesson1/go-stripe/cmd/web/ws-handlers.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/gorilla/websocket"
 
-type WebSocketConnection struct (
+type WebSocketConnection struct {
 	*websocket.Conn
-)
+}
+
+type WsPayload struct {
+	Action      string              `json: "action"`
+	Message     string              `json: "message"`
+	UserName    string              `json: "username"`
+	MessageType string              `json: "message_type"`
+	Conn        WebSocketConnection `json: "-"`
+}
