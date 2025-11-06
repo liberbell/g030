1a04e6697850edb752a2b8d1252d5bd1190fd9bd
Liberbell <liberbell@gmail.com>
2025-11-07 08:36:54 +0900

    invoice2

--- lesson1/go-stripe/cmd/micro/invoice/invoice.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoice.go
@@ -1,1 +1,23 @@
 package main
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+	}
+	secretkey string
+	frontend  string
+}
