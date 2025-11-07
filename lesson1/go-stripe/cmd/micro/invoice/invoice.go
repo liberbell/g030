3eea11cf31e22c443bf8be640c483f1bd2a9d0cc
Liberbell <liberbell@gmail.com>
2025-11-07 09:20:44 +0900

    invoice4

--- lesson1/go-stripe/cmd/micro/invoice/invoice.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoice.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"os"
+)
 
 const version = "1.0.0"
 
@@ -20,4 +24,17 @@ type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
 	version  string
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 5000, "Server port to listen on")
+	flag.StringVar(&cfg.smtp.host, "smtphost", "sandbox.smtp.mailtrap.io", "smtp host")
+	cfg.smtp.username = os.Getenv("MAIL_User")
+	cfg.smtp.password = os.Getenv("MAIL_Pass")
+	flag.IntVar(&cfg.smtp.port, "smtpport", 587, "Smtp Port")
+	flag.StringVar(&cfg.frontend, "frontend", "http://localhost:4000", "url to front end")
+
+	flag.Parse()
 }
