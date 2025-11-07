1b4020650b5e8da4b4db66339b5933f38b3413b3
Liberbell <liberbell@gmail.com>
2025-11-08 08:59:20 +0900

    invoce-routes1

--- lesson1/go-stripe/cmd/micro/invoice/invoce-routes.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoce-routes.go
@@ -18,7 +18,7 @@ func (app *application) routes() http.Handler {
 		MaxAge:           100,
 	}))
 
-	mux.Get("/invoce/create-and-send", app.CreateAndSendInvoce)
+	mux.Post("/invoce/create-and-send", app.CreateAndSendInvoce)
 
 	return mux
 }
