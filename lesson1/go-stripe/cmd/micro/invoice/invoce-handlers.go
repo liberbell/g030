b7f621e38ace9eee788c70692243665c2a37d59a
Liberbell <liberbell@gmail.com>
2025-11-08 09:05:37 +0900

    invoice-handlers3

--- lesson1/go-stripe/cmd/micro/invoice/invoce-handlers.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoce-handlers.go
@@ -6,15 +6,11 @@ import (
 )
 
 type Order struct {
-	ID          int         `json: "id"`
-	Quantity    int         `json: "quantity"`
-	Amount      int         `json: "amount"`
-	Product     string      `json: "product"`
-	CreatedAt   time.Time   `json: "-"`
-	UpdatedAt   time.Time   `json: "-"`
-	Widget      Widget      `json: "widget"`
-	Transaction Transaction `json: "transaction"`
-	Customer    Customer    `json: "customer"`
+	ID        int       `json: "id"`
+	Quantity  int       `json: "quantity"`
+	Amount    int       `json: "amount"`
+	Product   string    `json: "product"`
+	CreatedAt time.Time `json: "created_at"`
 }
 
 func (app *application) CreateAndSendInvoce(w http.ResponseWriter, r *http.Request) {
