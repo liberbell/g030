3a0097e6e644c903ae5d7584b50fb879be7450ed
Liberbell <liberbell@gmail.com>
2025-11-08 09:09:56 +0900

    invoice-handlers4

--- lesson1/go-stripe/cmd/micro/invoice/invoice-handlers.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoice-handlers.go
@@ -10,6 +10,9 @@ type Order struct {
 	Quantity  int       `json: "quantity"`
 	Amount    int       `json: "amount"`
 	Product   string    `json: "product"`
+	FirstName string    `json: "first_name"`
+	LastName  string    `json: "last_name"`
+	FirstName string    `json: "first_name"`
 	CreatedAt time.Time `json: "created_at"`
 }
 
