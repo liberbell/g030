fbbdaf1252ea7804b42af8f11594c8d904c24b8c
Liberbell <liberbell@gmail.com>
2025-11-08 09:04:16 +0900

    invoice-handlers2

--- lesson1/go-stripe/cmd/micro/invoice/invoce-handlers.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoce-handlers.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
 )
 
+type Order struct {
+	ID          int         `json: "id"`
+	Quantity    int         `json: "quantity"`
+	Amount      int         `json: "amount"`
+	Product     string      `json: "product"`
+	CreatedAt   time.Time   `json: "-"`
+	UpdatedAt   time.Time   `json: "-"`
+	Widget      Widget      `json: "widget"`
+	Transaction Transaction `json: "transaction"`
+	Customer    Customer    `json: "customer"`
+}
+
 func (app *application) CreateAndSendInvoce(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Heelo %s", "world.")
+
 }
