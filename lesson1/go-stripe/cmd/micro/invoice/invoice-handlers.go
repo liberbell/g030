9007991ab18311ba707302bfefd85b0a4dec300a
Liberbell <liberbell@gmail.com>
2025-11-09 10:19:38 +0900

    invoice-handlers2

--- lesson1/go-stripe/cmd/micro/invoice/invoice-handlers.go
+++ lesson1/go-stripe/cmd/micro/invoice/invoice-handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -17,5 +18,18 @@ type Order struct {
 }
 
 func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
+	var order Order
 
+	err := app.readJSON(w, r, &order)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	var resp struct {
+		Error   bool   `json: "error"`
+		Message string `json: "message"`
+	}
+	resp.Error = false
+	resp.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
 }
