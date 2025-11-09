e80631ef7f261f9e159fe57318f313bb84ee1843
Liberbell <liberbell@gmail.com>
2025-11-10 08:44:33 +0900

    helpers4

--- lesson1/go-stripe/cmd/micro/invoice/helpers.go
+++ lesson1/go-stripe/cmd/micro/invoice/helpers.go
@@ -65,5 +65,10 @@ func (app *application) CreateDirIfNotExist(path string) error {
 	const mode = 0755
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err := os.Mkdir(path, mode)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
 	}
+	return nil
 }
