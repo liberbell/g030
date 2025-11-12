5e7ba12aec070009a04d3cd30e2e72788df1f349
Liberbell <liberbell@gmail.com>
2025-11-13 08:19:30 +0900

    handlers3

--- lesson1/go-stripe/cmd/web/handlers.go
+++ lesson1/go-stripe/cmd/web/handlers.go
@@ -94,12 +94,14 @@ func (app *application) GetTransactionData(r *http.Request) (TransactionData, er
 }
 
 type Order struct {
-	ID        int    `json: "id"`
-	Quantity  int    `json: "quantity"`
-	Amount    int    `json: "amount"`
-	Product   string `json: "product"`
-	FirstName string `json: "first_name"`
-	LastName  string `json: "last_name"`
+	ID        int       `json: "id"`
+	Quantity  int       `json: "quantity"`
+	Amount    int       `json: "amount"`
+	Product   string    `json: "product"`
+	FirstName string    `json: "first_name"`
+	LastName  string    `json: "last_name"`
+	Email     string    `json: "email"`
+	CreatedAt time.Time `json: "created_at"`
 }
 
 func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
